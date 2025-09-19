15f665c8355edec18cd2a0e5ca18801ca446a235
Kent Tonino <tonino.kentlouise@gmail.com>
2025-09-19 21:14:11 +0800

    refactor: change add_new_line param name
    This make sure the variable is understandble.
    
    Signed-off-by: Kent Tonino <tonino.kentlouise@gmail.com>

--- src/displays/escape_sequence.c
+++ src/displays/escape_sequence.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void add_new_line(int num) {
-  for (int i = 0; i < num; i++) {
+void add_new_line(int num_lines) {
+  for (int i = 0; i < num_lines; i++) {
     printf("\n");
   }
 }
