570618b9000b7e229862eb08719b16c6e6912d82
Kent Tonino <tonino.kentlouise@gmail.com>
2026-03-22 20:34:42 +0800

    chore: format

--- src/scales/celsius_option.c
+++ src/scales/celsius_option.c
@@ -47,22 +47,19 @@ void celsius_option(void) {
     // * Handle computation for fahrenheit.
     double fahrenheit = ((9.0 / 5.0) * celsius_double) + 32;
     add_new_tab(1);
-    printf(" %sFahrenheit (°F)%s  : %s%.4lf (°F)%s ", BLUE, RESET, YELLOW,
-           fahrenheit, RESET);
+    printf(" %sFahrenheit (°F)%s  : %s%.4lf (°F)%s ", BLUE, RESET, YELLOW, fahrenheit, RESET);
     add_new_line(1);
 
     // * Handle computation for kelvbin
     double kelvin = celsius_double + 273.15;
     add_new_tab(1);
-    printf(" %sKelvin (°K)%s      : %s%.4lf (°K)%s", BLUE, RESET, YELLOW,
-           kelvin, RESET);
+    printf(" %sKelvin (°K)%s      : %s%.4lf (°K)%s", BLUE, RESET, YELLOW, kelvin, RESET);
     add_new_line(1);
 
     // * Handle computation for rankine.
     double rankine = celsius_double * (9.0 / 5.0) + 491.67;
     add_new_tab(1);
-    printf(" %sRankine (°R)%s     : %s%.4lf (°R)%s", BLUE, RESET, YELLOW,
-           rankine, RESET);
+    printf(" %sRankine (°R)%s     : %s%.4lf (°R)%s", BLUE, RESET, YELLOW, rankine, RESET);
     add_new_line(2);
 
     // * Ask user if want to try again.
