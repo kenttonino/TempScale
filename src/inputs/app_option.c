849bc63d087637a89e17ed3a94c2717e07fbbad7
Kent Tonino <tonino.kentlouise@gmail.com>
2026-03-22 20:38:14 +0800

    fix: app_option memory leak

--- src/inputs/app_option.c
+++ src/inputs/app_option.c
@@ -1,15 +1,14 @@
 #include "../displays/ansi_color_codes.h"
 #include "../displays/escape_sequence.h"
-#include <stdlib.h>
 
 int app_option(void) {
   int option = 0;
-  char *garbage_buffer = malloc(sizeof(char) * 50);
+  static char garbage_buffer[1000];
 
   add_new_tab(1);
   printf(" %sEnter the option:%s ", BLUE, RESET);
   scanf("%d", &option);
-  fgets(garbage_buffer, 50, stdin);
+  fgets(garbage_buffer, sizeof(garbage_buffer), stdin);
 
   return option;
 }
